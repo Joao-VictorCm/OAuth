81b7c09367f41a4fb06a5e913f89312135ea154e
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-12-31 13:54:53 -0300

    add login com o google

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import session from "express-session";
+import GoogleStrategy from "passport-google-oauth2"
 import env from "dotenv";
 
 const app = express();
@@ -64,6 +65,21 @@ app.get("/secrets", (req, res) => {
     res.redirect("/login");
   }
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google",{
+    scope: ["profile", "email"],
+
+  })
+)
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+)
 
 app.post(
   "/login",
@@ -138,6 +154,28 @@ passport.use(
     }
   })
 );
+
+passport.use("google", new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "http://www.googleleapis.com/oauth2/v3/userinfo"
+}, async (accessToken, refreshToken, profile, cb) =>{
+  console.log(profile)
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [profile.email])
+    if(result.rows.length === 0){
+      const newUser = await db.query("INSERT INTO users (email, password) VALUES (S1, $2)", 
+        [profile.email, "google"]
+      )
+      cb(null, newUser.rows[0])
+    }else{
+      cb(null, result.rows[0])
+    }
+  } catch (err) {
+    cb(err)
+  }
+}))
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
